703be9d5e36791c7869726a5251304df1b99fe11
Jared Harper <har18082@byui.edu>
2020-04-06 09:39:32 -0400

    fixed test

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-console.log(process.env.DATABASE_URL);
-/*
+//console.log();
+
 const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-        client: 'pg',
-        connection: process.env.DATABASE_URLs
-    }
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   });
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,25 +17,8 @@ app.use(express.static('www'));
 app.use(express.urlencoded({ extended: true }));
 
 
-class dummyUser 
-{
-  
-    constructor(username, password)
-    {
-        this.password = password;
-        this.username = username;
-    }
-}
-
-var dummyUsers = Array();
-
-var user1 = new dummyUser('Prime',"it's_prime_time");
-var user2 = new dummyUser("TMNT", "Turtle_Power");
-var user3 = new dummyUser("Thing","its_clobberin'_time");
-var user4 = new dummyUser("Darkwing","let's_get_dangerous");
 
 
-dummyUsers.push(user1,user2,user3,user4);
 
 
 
@@ -46,15 +26,7 @@ dummyUsers.push(user1,user2,user3,user4);
 
 app.post('/login', function (req,res) {
     var theUser = "Invalid Login";
-    console.log(JSON.stringify(req.body));
-    for (let index = 0; index < dummyUsers.length; index++) {
-        const element = dummyUsers[index];
-        console.log(JSON.stringify(element));
-        if (element.username == req.body.username ) {
-            console.log(element.password == req.body.password);
-            theUser = element.username;
-        }      
-    }
+   
 
         res.render('dummypage',{'username':theUser});
 
